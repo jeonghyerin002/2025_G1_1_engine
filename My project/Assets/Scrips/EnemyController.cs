2bced9845ef7b327fba0c07633388ecf1c1ca011
jeonghyerin002 <jhr102446@gmail.com>
2025-04-17 12:28:53 +0900

    [Feat] Git 설정 파일 변경

---
+++ My project/Assets/Scrips/EnemyController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed = 3.5f;
+
+    private Rigidbody2D  rb;
+    private bool isMovingRigjt = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMovingRigjt)
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        else
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Boundary"))
+        {
+            isMovingRigjt = !isMovingRigjt;
+
+            Vector3 localscale = transform.localScale;
+            localscale.x *= -1;
+            transform.localScale = localscale;
+        }
+    }
+}
